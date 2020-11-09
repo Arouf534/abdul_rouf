e150b7a7926bde70bcde1c46244776ca8395e92e
Arouf534 <73456192+Arouf534@users.noreply.github.com>
2020-11-09 21:28:07 +0530

    Add files via upload

---
+++ yelpcamp_v3/aaa.js
@@ -0,0 +1,37 @@
+var mongoose=require('mongoose');
+
+mongoose.connect("mongodb://localhost/campgrounds");
+
+var campgroundschema= new mongoose.Schema({
+    name:String,
+    image:String
+})
+
+ var Campground=mongoose.model("Campground",campgroundschema)
+
+
+
+
+var camps=[{name:'camp1',image:"https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+{name:'camp2',  image:"https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+,{name:'camp2',  image:"https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+,{name:'camp2',  image:"https://images.unsplash.com/photo-1520095972714-909e91b038e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+,{name:'camp1',image:"https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+{name:'camp2',  image:"https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+,{name:'camp2',  image:"https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+,{name:'camp2',  image:"https://images.unsplash.com/photo-1520095972714-909e91b038e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+]
+
+
+camps.forEach(element => {
+
+
+    Campground.create(element,function(err,camp){
+        if(err){
+            console.log(err)
+        }else{
+            console.log(camp)
+        }
+    })
+    
+});
